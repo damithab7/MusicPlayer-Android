d1e9baf3836049da55eba26ebf401b17480b3753
Damitha Sulochana <damitha.sb@gmail.com>
2026-03-15 09:32:32 +0530

    feat: login & player layout and themes

---
+++ app/src/main/java/lk/damithab/musicplayer/activity/LoginActivity.java
@@ -0,0 +1,29 @@
+package lk.damithab.musicplayer.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import lk.damithab.musicplayer.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        this.loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(V->{
+
+        });
+    }
+}
